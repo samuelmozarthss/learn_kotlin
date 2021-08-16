79ac48c6fc5a4e4a9823c8d8f9c05085a99cf05c
Samuel Mozarth <65967088+samuelmozarthss@users.noreply.github.com>
2021-08-16 17:11:35 -0300

    while

---
+++ src/flowWhile.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+//    while (true) {
+//        println("Gabriel")
+//    }
+//    Cuidado com os loops
+    var index: Int = 0
+    while (index <= 100) {
+        println(index)
+        index++
+
+        if (index == 50) {
+            break
+        }
+   }
+}
